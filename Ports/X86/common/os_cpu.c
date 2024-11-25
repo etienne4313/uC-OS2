6339616b49b099d57d567f4af90f1b847842e067
Etienne <etienne4313@gmail.com>
2024-11-25 07:49:57 -0500

    Minor fixes - Remove Config.mk for AVR target - Add KLM load and unload support - Proper module_get/put for KLM - Doc update
    Signed-off-by: Etienne <etienne4313@gmail.com>

--- Ports/X86/common/os_cpu.c
+++ Ports/X86/common/os_cpu.c
@@ -30,9 +30,16 @@ void OSTaskReturnHook(OS_TCB *ptcb){}
 void OSSystemReset(void){}
 void OSTCBInitHook(OS_TCB *ptcb){}
 void OSTimeTickHook (void){}
-void OSInitHookBegin(void){}
 void OSInitHookEnd(void){}
 void OSTaskIdleHook(void){}
+
+void OSInitHookBegin(void)
+{
+#ifdef __KERNEL__
+	/* From this point on, don't let the module go unless the RTOS has terminated */
+	__module_get(THIS_MODULE);
+#endif
+}
 
 OS_STK* OSTaskStkInit (void (*task)(void* pd), void* pdata, OS_STK* ptos, INT16U opt)
 {
@@ -159,13 +166,34 @@ void exit_critical(void)
 }
 
 #ifdef __KERNEL__
+
+/*
+ * When the module is loaded, *rtos_entry is set to main()
+ * Then upon taking the CPU offline using
+ * 	echo 0 >/sys/devices/system/cpu/cpu'n'/online
+ * the dying CPU will call rtos_entry(); just before going in mwait()
+ * from native_play_dead()
+ * Befor trying to wakeup the CPU, the RTOS needs to be terminated by
+ * reading /proc/rtos which will set the stack back to the original value
+ * and allow native_play_dead() to continue it's original execution. In
+ * other words, *rtos_entry will return back to the caller but it will
+ * first go thru HANDLE_EXIT
+ *
+ * Then at that point the CPU can be taken online using
+ *  echo 1 >/sys/devices/system/cpu/cpu'n'/online
+ *
+ * Somehow the CPU can be taken back online without terminating the RTOS
+ *  This is specific to the CPU type / feature
+ */
 static int rtos_kill(struct seq_file *m, void *v)
 {
 	seq_puts(m, "RTOS kill");
 	seq_putc(m, '\n');
 	rtos_dead = 1;
+	module_put(THIS_MODULE);
 	return 0;
 }
+
 static int __init init(void)
 {
 	rtos_dead = 0;
@@ -173,11 +201,13 @@ static int __init init(void)
 	proc_create_single("rtos", 0, NULL, rtos_kill);
 	return 0;
 }
+
 static void __exit fini(void)
 {
 	remove_proc_entry("rtos", NULL);
 	rtos_entry = NULL;
 }
+
 module_init(init);
 module_exit(fini);
 MODULE_LICENSE("GPL v2");
